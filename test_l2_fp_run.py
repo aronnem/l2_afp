d545e49133fbcdcdb7fe9ccbe593e6d91ad4bf69
Aronne Merrelli <aronne.merrelli@ssec.wisc.edu>
2018-03-07 23:27:52 +0000

    change to more closely mimic l2_special_run, and to use the L2run directly

--- test_l2_fp_run.py
+++ test_l2_fp_run.py
@@ -1,7 +1,8 @@
-import os.path
+import os, shutil
 
-import l2_full_physics_wrapper
-import retrieval
+#import l2_full_physics_wrapper
+#import retrieval
+import full_physics
 
 ddir = '/data/merrelli/OCO2_L2_workarea/sandbox_B8_pytest'
 L1bfile = os.path.join(ddir, 'oco2_L1bScTG_06280a_150906_B7000r_151030071317.h5')
@@ -13,14 +14,35 @@ config_file = 'custom_config.lua'
 merradir = '/data/OCO2/L2_datasets/merra_composite'
 abscodir = '/data/OCO2/absco'
 
-l2_obj = l2_full_physics_wrapper.wrapped_l2_fp(
-    L1bfile, ECfile, config_file, merradir, abscodir, 
-    sounding_id = sounding_id, imap_file = IDPfile)
+#l2_obj = l2_full_physics_wrapper.wrapped_l2_fp(
+#    L1bfile, ECfile, config_file, merradir, abscodir, 
+#    sounding_id = sounding_id, imap_file = IDPfile)
 
-Sa = l2_obj.get_Sa()
-Se_diag = l2_obj.get_Se_diag()
-Kupdate = l2_obj.Kupdate
-y = l2_obj.get_y()
-x0 = l2_obj.get_x()
+#Sa = l2_obj.get_Sa()
+#Se_diag = l2_obj.get_Se_diag()
+#Kupdate = l2_obj.Kupdate
+#y = l2_obj.get_y()
+#x0 = l2_obj.get_x()
 
-l2_solver_res = l2_obj._L2run.solver.solve(x0.copy(), x0.copy(), Sa.copy())
+# copying output method from l2_special_run.py
+L2run = full_physics.L2Run(
+    config_file, sounding_id, ECfile, L1bfile, 
+    abscodir = abscodir, merradir = merradir, 
+    imap_file = IDPfile)
+
+x_a = L2run.state_vector.state.copy()
+x_i = x_a.copy()
+cov_a = L2run.state_vector.state_covariance.copy()
+
+out, outerr = L2run.config.output()
+l2_solver_res = L2run.solver.solve(x_i, x_a, cov_a)
+out.write()
+out = None
+L2run = None
+
+output_filename = 'l2fp_test_'+sounding_id+'.h5' 
+try:
+    os.rename('out.h5', output_filename)
+except OSError:
+    print('out.h5 not found, copying the generating file')
+    shutil.copyfile('out.h5.generating', output_filename)
