bbf725322526bfd082b8f24ff39c07b13551a72e
Aronne Merrelli <aronne.merrelli@ssec.wisc.edu>
2021-07-21 21:27:15 +0000

    update the ABSCO 5.0 paths after change on my local filesystem I replaced the "+" in the filenames and changed hdf to h5

--- l2_afp/lua_configs/custom_config_absco42.lua
+++ l2_afp/lua_configs/custom_config_absco42.lua
@@ -29,9 +29,9 @@ config.fm.atmosphere.absorber.O2.absco = "v4.2.0_unscaled/o2_v4.2.0_drouin.hdf"
 config.fm.atmosphere.absorber.CO2.absco = "v4.2.0_unscaled/co2_v4.2.0_with_ctm.hdf"
 config.fm.atmosphere.absorber.H2O.absco = "v4.2.0_unscaled/h2o_v4.2.0.hdf"
 
---config.fm.atmosphere.absorber.O2.absco = "v5.0.0/o2_v151005_cia_mlawer_v151005r1_narrow.hdf"
---config.fm.atmosphere.absorber.CO2.absco = "v5.0.0/co2_devi2015_wco2scale=nist_sco2scale=unity.hdf"
---config.fm.atmosphere.absorber.H2O.absco = "v5.0.0/h2o_hitran12.hdf"
+--config.fm.atmosphere.absorber.O2.absco = "v5.0.0/o2_v151005_cia_mlawer_v151005r1_narrow.h5"
+--config.fm.atmosphere.absorber.CO2.absco = "v5.0.0/co2_devi2015_wco2scale-nist_sco2scale-unity.h5"
+--config.fm.atmosphere.absorber.H2O.absco = "v5.0.0/h2o_hitran12.h5"
 
 --config.fm.atmosphere.absorber.O2.table_scale = 1.0
 --config.fm.atmosphere.absorber.CO2.table_scale = { 1.0, 1.0, 1.004 }
--- l2_afp/lua_configs/custom_config_fixed_aerosol.lua
+++ l2_afp/lua_configs/custom_config_fixed_aerosol.lua
@@ -4,9 +4,9 @@ require "oco_base_config"
 
 config = OcoBaseConfig:new()
 
-config.fm.atmosphere.absorber.O2.absco = "v5.0.0/o2_v151005_cia_mlawer_v151005r1_narrow.hdf"
-config.fm.atmosphere.absorber.CO2.absco = "v5.0.0/co2_devi2015_wco2scale=nist_sco2scale=unity.hdf"
-config.fm.atmosphere.absorber.H2O.absco = "v5.0.0/h2o_hitran12.hdf"
+config.fm.atmosphere.absorber.O2.absco = "v5.0.0/o2_v151005_cia_mlawer_v151005r1_narrow.h5"
+config.fm.atmosphere.absorber.CO2.absco = "v5.0.0/co2_devi2015_wco2scale-nist_sco2scale-unity.h5"
+config.fm.atmosphere.absorber.H2O.absco = "v5.0.0/h2o_hitran12.h5"
 
 -- turn on per-iteration output
 config.iteration_output = true
